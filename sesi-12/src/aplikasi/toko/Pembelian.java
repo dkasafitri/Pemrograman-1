cfb92513ba5478e22f7f001cd2c5cc4d704c1eab
Dika Ayu Safitri <dikasafitri93@yahoo.co.id>
2011-12-21 14:37:27 +0700

    Pemrograman 1 sesi-12

---
+++ sesi-12/src/aplikasi/toko/Pembelian.java
@@ -0,0 +1,10 @@
+package aplikasi.toko;
+
+import java.util.Date;
+import java.util.List;
+
+public class Pembelian
+{
+	private Date tanggalTransaksi;
+	private List <PembelianDetail> daftarPembelianDetail;
+}
---
+++ sesi-12/src/aplikasi/toko/Produk.java
@@ -0,0 +1,10 @@
+package aplikasi.toko;
+
+import java.math.BigDecimal;
+
+public class Produk
+{
+	private String kode;
+	private String nama;
+	private BigDecimal harga;
+}
